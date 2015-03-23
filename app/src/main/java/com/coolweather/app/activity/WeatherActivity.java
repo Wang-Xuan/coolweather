9321ee81b1ae6877f97fe472b40227e27dde5cf6
Wang-Xuan <wxlayun36@gmail.com>
2015-03-23 16:55:42 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/coolweather/app/activity/WeatherActivity.java
+++ app/src/main/java/com/coolweather/app/activity/WeatherActivity.java
@@ -1,12 +1,14 @@
 package com.coolweather.app.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -18,7 +20,7 @@ import com.coolweather.app.util.Utility;
 /**
  * Created by lixfe on 2015/3/23.
  */
-public class WeatherActivity extends Activity{
+public class WeatherActivity extends Activity implements View.OnClickListener {
 
     private LinearLayout weatherInfoLayout;
 
@@ -40,6 +42,12 @@ public class WeatherActivity extends Activity{
     // 用于显示气温2
     private TextView temp2Text;
 
+    // 切换城市按钮
+    private Button switchCity;
+
+    // 更新天气按钮
+    private Button refreshWeather;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -54,6 +62,10 @@ public class WeatherActivity extends Activity{
         weatherDespText = (TextView) findViewById(R.id.weather_desp);
         temp1Text = (TextView) findViewById(R.id.temp1);
         temp2Text = (TextView) findViewById(R.id.temp2);
+        switchCity = (Button) findViewById(R.id.switch_city);
+        refreshWeather = (Button) findViewById(R.id.refresh_weather);
+        switchCity.setOnClickListener(this);
+        refreshWeather.setOnClickListener(this);
 
         String countyCode = getIntent().getStringExtra("county_code");
         if(!TextUtils.isEmpty(countyCode)) {
@@ -65,6 +77,29 @@ public class WeatherActivity extends Activity{
         } else {
             // 没有县级代码时就直接显示本地天气
             showWeather();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.switch_city:
+                Intent intent = new Intent(this, ChooseAreaActivity.class);
+                intent.putExtra("from_weather_activity", true);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.refresh_weather:
+                publishText.setText("同步中...");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String weatherCode = prefs.getString("weather_code", "");
+                if (!TextUtils.isEmpty(weatherCode)) {
+                    queryWeatherInfo(weatherCode);
+                }
+                break;
+            default:
+                break;
         }
     }
 
@@ -141,5 +176,4 @@ public class WeatherActivity extends Activity{
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
     }
-
 }
